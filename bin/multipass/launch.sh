d24273a0710b3df1619d1c36289af701f23c0910
koockram <koockram@gmail.com>
2022-10-30 12:11:32 +0000

    revised multipass launch scripts

---
+++ bin/multipass/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# start or stop or create off the shelf lts ubuntu image which is configured for docker
+# all based on assumption that "multipass" from ubuntu is installed on mac
+
+VM=$1
+NM=${2:-$1}
+
+if [ $(which multipass) ]
+then
+	if [ ! "$(multipass list | grep ^$VM)" ]
+	then
+		multipass launch --name $NM --mount $HOME:$HOME $VM
+	fi
+
+	multipass info $NM
+else
+	echo "WARN: executable multipass not installed, exiting..."
+	exit 1
+fi
